60dafb57fed71c7a7e0b394959e54854e2e721b2
garibcatastrofe <ramsesgaribflorescuen@gmail.com>
2026-08-30 23:23:03 -0700

    feat: :sparkles: Funcionalidad basica de actualizar reporte terminada

--- content/report/ReportContent.tsx
+++ content/report/ReportContent.tsx
@@ -7,6 +7,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 /* HOOKS */
 import { useReport } from "./hooks/useReport";
 
+/* ICONS */
+import {
+  Check,
+  CircleQuestionMark,
+  Minus,
+  SquarePen,
+  X,
+} from "lucide-react-native";
+
 /* NAVIGATION */
 import { useLocalSearchParams, useRouter } from "expo-router";
 
@@ -18,23 +27,25 @@ import { useTheme } from "@/theme/ThemeContext";
 
 /* UTILS */
 import { formatDate } from "@/content/shared/utils/formatDate";
+import { useEffect } from "react";
 
 export function ReportContent() {
   const router = useRouter();
   const { id } = useLocalSearchParams<{ id: string }>();
 
   const { theme } = useTheme();
-  const { report } = useReport(id);
+  const { report, changeMealStatus, updateReport, updating } = useReport(id);
+
+  useEffect(() => {
+    console.log(report);
+  }, [report]);
 
   return (
     <SafeAreaView
       className="flex-col justify-center flex-1"
       style={{ backgroundColor: theme.background }}
     >
-      <ScrollView
-        className="flex-1 py-6"
-        contentContainerClassName="flex-col gap-4"
-      >
+      <ScrollView className="flex-1 py-6" contentContainerClassName="gap-4">
         <View className="flex-row items-center mx-6">
           <Pressable onPress={() => router.back()}>
             <ArrowLeft size={24} color={theme.body} />
@@ -43,28 +54,146 @@ export function ReportContent() {
         </View>
 
         <TextApp
-          className="mx-6 mb-4 text-xl"
+          className="mx-6 mb-2 text-xl"
           style={{ fontFamily: "DMSans_500Medium" }}
         >
           ¿Cómo fueron tus comidas este día?
         </TextApp>
 
         {!report ? (
-          <View>
+          <View className="gap-4 mx-6">
             <TextApp>Cargando...</TextApp>
           </View>
         ) : (
-          <View className="gap-4">
-            <TextApp>Fecha</TextApp>
-            <View className="flex-row gap-4">
+          <View className="gap-4 mx-6">
+            <TextApp className="text-lg">Fecha</TextApp>
+            <View className="flex-row items-center gap-4">
               <Calendar size={20} color={theme.ink} />
               <TextApp>{formatDate(report.date)}</TextApp>
             </View>
 
-            <TextApp>Desayuno</TextApp>
+            <MealStatusSelector
+              label="Desayuno"
+              meal="desayuno"
+              status={report.breakfast_status}
+              changeMealStatus={changeMealStatus}
+            />
+            <MealStatusSelector
+              label="Comida"
+              meal="comida"
+              status={report.lunch_status}
+              changeMealStatus={changeMealStatus}
+            />
+            <MealStatusSelector
+              label="Cena"
+              meal="cena"
+              status={report.dinner_status}
+              changeMealStatus={changeMealStatus}
+            />
           </View>
         )}
       </ScrollView>
+
+      {report && (
+        <Pressable
+          className="flex-row items-center justify-center gap-4 px-4 py-4 mx-6 mb-6 rounded-full"
+          style={{
+            backgroundColor: updating ? theme.disabled_bg : theme.primary,
+          }}
+          onPress={!updating ? updateReport : undefined}
+        >
+          <SquarePen
+            size={20}
+            color={updating ? theme.disabled : theme.primary_txt}
+          />
+          <TextApp
+            className="text-lg"
+            style={{
+              fontFamily: "DMSans_700Bold",
+              color: updating ? theme.disabled : theme.primary_txt,
+            }}
+          >
+            Actualizar
+          </TextApp>
+        </Pressable>
+      )}
     </SafeAreaView>
+  );
+}
+
+function MealStatusSelector({
+  label,
+  meal,
+  status,
+  changeMealStatus,
+}: {
+  label: string;
+  meal: string;
+  status: string;
+  changeMealStatus: (status: string, meal: string) => void;
+}) {
+  const { theme } = useTheme();
+
+  return (
+    <View className="gap-4">
+      <TextApp className="text-lg">{label}</TextApp>
+
+      <View className="flex-row gap-4">
+        <Pressable
+          className="items-center justify-center flex-1 p-4 rounded-full"
+          style={{
+            backgroundColor:
+              status === "excelente" ? theme.success_bg : theme.surface,
+          }}
+          onPress={() => changeMealStatus("excelente", meal)}
+        >
+          <Check
+            size={20}
+            color={status === "excelente" ? theme.success : theme.muted}
+          />
+        </Pressable>
+
+        <Pressable
+          className="items-center justify-center flex-1 p-4 rounded-full"
+          style={{
+            backgroundColor:
+              status === "regular" ? theme.warn_bg : theme.surface,
+          }}
+          onPress={() => changeMealStatus("regular", meal)}
+        >
+          <Minus
+            size={20}
+            color={status === "regular" ? theme.warn : theme.muted}
+          />
+        </Pressable>
+
+        <Pressable
+          className="items-center justify-center flex-1 p-4 rounded-full"
+          style={{
+            backgroundColor:
+              status === "terrible" ? theme.danger_bg : theme.surface,
+          }}
+          onPress={() => changeMealStatus("terrible", meal)}
+        >
+          <X
+            size={20}
+            color={status === "terrible" ? theme.danger : theme.muted}
+          />
+        </Pressable>
+
+        <Pressable
+          className="items-center justify-center flex-1 p-4 rounded-full"
+          style={{
+            backgroundColor: status === "vacio" ? theme.info_bg : theme.surface,
+          }}
+          onPress={() => changeMealStatus("vacio", meal)}
+        >
+          <CircleQuestionMark
+            size={20}
+            color={status === "vacio" ? theme.info : theme.muted}
+          />
+        </Pressable>
+      </View>
+    </View>
   );
 }
--- content/report/hooks/useReport.ts
+++ content/report/hooks/useReport.ts
@@ -6,6 +6,44 @@ import { useEffect, useState } from "react";
 
 export function useReport(id: string) {
   const [report, setReport] = useState<IReportPrimitive | null>(null);
+  const [updating, setUpdating] = useState<boolean>(false);
+
+  const changeMealStatus = (status: string, meal: string) => {
+    if (!report) return;
+
+    if (meal === "desayuno") {
+      if (status === "excelente")
+        return setReport({ ...report, breakfast_status: "excelente" });
+      if (status === "regular")
+        return setReport({ ...report, breakfast_status: "regular" });
+      if (status === "terrible")
+        return setReport({ ...report, breakfast_status: "terrible" });
+
+      return setReport({ ...report, breakfast_status: "vacio" });
+    }
+
+    if (meal === "comida") {
+      if (status === "excelente")
+        return setReport({ ...report, lunch_status: "excelente" });
+      if (status === "regular")
+        return setReport({ ...report, lunch_status: "regular" });
+      if (status === "terrible")
+        return setReport({ ...report, lunch_status: "terrible" });
+
+      return setReport({ ...report, lunch_status: "vacio" });
+    }
+
+    if (meal === "cena") {
+      if (status === "excelente")
+        return setReport({ ...report, dinner_status: "excelente" });
+      if (status === "regular")
+        return setReport({ ...report, dinner_status: "regular" });
+      if (status === "terrible")
+        return setReport({ ...report, dinner_status: "terrible" });
+
+      return setReport({ ...report, dinner_status: "vacio" });
+    }
+  };
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -21,5 +59,18 @@ export function useReport(id: string) {
     return () => clearTimeout(timeout);
   }, []);
 
-  return { report };
+  const updateReport = async () => {
+    try {
+      setUpdating(true);
+      setTimeout(() => {
+        console.log("Reporte guardado/actualizado correctamente");
+        console.log(report);
+        setUpdating(false);
+      }, 3000);
+    } catch {
+      setUpdating(false);
+    }
+  };
+
+  return { report, changeMealStatus, updating, updateReport };
 }
